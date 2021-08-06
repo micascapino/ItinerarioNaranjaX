1c84904df6a9ecf6f6df5662b76a0f90abb1f412
Micaela Scapino <micascapinomdq@gmail.com>
2021-08-05 22:53:41 -0300

    mejora esquema del itinerario

--- models/itineraryModel.js
+++ models/itineraryModel.js
@@ -4,16 +4,24 @@ const mongoose = require("mongoose");
 const itinerarySchema = new mongoose.Schema ({
 	title: { type: String, required: true },
 	img: { type: String, required: true },
-	activities: { type: Object },
+	activities: { 
+		name: {type: String},
+		img: {type: String}
+	},
 	authorName: { type: String, required: true },
 	authorPic: { type: String, required: true },
 	price: { type: Number, required: true, min: 1, max: 5 },
 	duration: { type: Number, required: true, min: 1 },
 	likes: { type: Number, default: 0 },
 	hashtags: { type: [String] },
-	comments: { type: Object },
+	comments: {
+		userId: {type: mongoose.Schema.Types.ObjectId, ref: "user"},
+		text: {type: String},
+		userName: {type: String},
+		userPic: {type: String}
+	},
 	usersLike: { type: [String] },
-	cityId: { type: Object, required: true }
+	cityId: { type: mongoose.Schema.Types.ObjectId, required: true }
 }); 
 
 module.exports = mongoose.model ("itinerary", itinerarySchema);
