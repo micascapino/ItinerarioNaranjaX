d90ace65a9a7fe520cd85f0d783ff7bf85943de5
Micaela Scapino <micascapinomdq@gmail.com>
2021-08-06 22:39:45 -0300

    Arreglo en eslint, remplazo tabs por spaces 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
 		"ecmaVersion": 12
 	},
 	"rules": { 
-		"indent": ["error","tab"],
+		"indent": ["error","spaces",2],
 		"linebreak-style": ["error","windows"],
 		"quotes": ["error","double"],
 		"semi": ["error","always"]
