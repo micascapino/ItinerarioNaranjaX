b3d2b0905147b300106b24cdcf4f3a0f2b3e4fd3
Micaela Scapino <micascapinomdq@gmail.com>
2021-08-12 21:33:22 -0300

    correcciones para que conecte con el front

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTop
 	.catch(err => console.log (err));
 
 //uso de los modelos y sus rutas de acceso
-app.use("/api/cities", require ("./routes/cities"));
+app.use("/api", require ("./routes/cities"));
 app.use("/api/itineraries", require ("./routes/itineraries"));
 app.use("/api/user", require ("./routes/users"));
 
